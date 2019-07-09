21e9711dfa5b04e895d068b0cd49448736489811
juliendcode <juliendcode@gmail.com>
2019-07-09 11:25:06 +0200

    config ssr

--- server.ts
+++ server.ts
@@ -1,33 +1,22 @@
-/**
- * *** NOTE ON IMPORTING FROM ANGULAR AND NGUNIVERSAL IN THIS FILE ***
- *
- * If your application uses third-party dependencies, you'll need to
- * either use Webpack or the Angular CLI's `bundleDependencies` feature
- * in order to adequately package them for use on the server without a
- * node_modules directory.
- *
- * However, due to the nature of the CLI's `bundleDependencies`, importing
- * Angular in this file will create a different instance of Angular than
- * the version in the compiled application code. This leads to unavoidable
- * conflicts. Therefore, please do not explicitly import from @angular or
- * @nguniversal in this file. You can export any needed resources
- * from your application's main.server.ts file, as seen below with the
- * import for `ngExpressEngine`.
- */
-
-import 'zone.js/dist/zone-node';
-
+import { enableProdMode } from '@angular/core';
+import { renderModuleFactory } from '@angular/platform-server';
 import * as express from 'express';
-import {join} from 'path';
-
-// Express server
-const app = express();
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import 'reflect-metadata';
+import 'zone.js/dist/zone-node';
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
-
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+const mode = (process.argv[2] === '--dev') ? 'development' : 'production';
+const app = express();
+if (mode === 'production') {
+  enableProdMode();
+  app.disable('x-powered-by');
+}
+
+
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -5,13 +5,18 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpClientXsrfModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     AppModule,
     BrowserModule.withServerTransition({ appId: 'space' }),
     BrowserTransferStateModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'csrftoken',
+      headerName: 'X-CSRFToken'
+    }),
   ],
   bootstrap: [AppComponent],
 })
